c3d3104ce452db6e456f033628105e8a5085cb5c
Pedro Barbosa <psbpedrobarbosa@gmail.com>
2015-01-08 17:12:20 +0000

    new scripts to process multiple bam/sam for sga scaffolder

---
+++ scaffolding/sga-scaffolder/bam2de.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#This script creates multiple .de files from bam.
+
+display_usage(){
+printf "First argument must be the file containing a list of bam files to process.
+Second argument is optional. It refers to the minimum number of pairs to make a contig-contig link. Default value is 5.\n"
+}
+
+if [ -z "$1" ]; then
+	printf "Please provide the required file.\n"
+	display_usage
+	exit 1
+fi
+
+if [ -z "$2" ]; then
+	min_pairs=5
+else
+	min_pairs="$2"
+fi
+
+while read line
+do	
+	printf "Processing $line file ..\n"
+	command="/opt/tools/sga/bin/sga-bam2de.pl -n $min_pairs --prefix ${line/.bam/} -m 200 $line"
+	$command
+done < $1
+printf "Done.\n"
