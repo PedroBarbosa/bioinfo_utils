99e4c1f64e3b395d4519fc4261f6654f12b8948c
pedro <psbpedrobarbosa@gmail.com>
2015-01-21 14:45:35 +0000

    new structuration with gitignore

--- preprocessing/mp-libraries/reverseComplement.py
+++ preprocessing/mp-libraries/reverseComplement.py
@@ -5,8 +5,9 @@ import sys
 import subprocess
 import os
 
-#This script performs the reverse complement of a list of files and automatically creates the new file in the 'revcomp' directory automatically generated.
-#It uses seqtk tool to do that.
+# This script performs the reverse complement of a list of files and automatically creates the new file in the 'revcomp'
+# directory automatically generated.
+# It uses seqtk tool to do that.
 
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
@@ -18,35 +19,34 @@ class MyParser(argparse.ArgumentParser):
 def revComp(inputFile):
     files_processed = 0
     seqtk_path = "/opt/tools/seqtk/seqtk seq -r"
-    output_str=""
+    output_str = ""
     with open(inputFile[0]) as file:
-	
-	if not os.path.exists("revcomp"):
-    		os.makedirs("revcomp")		
+
+        if not os.path.exists("revcomp"):
+            os.makedirs("revcomp")
+
         for line in file:
-	    output_str=""
+            output_str = ""
             if '.fq' in line:
                 #reformat file name
-		split_list = line.split("/")
-		filename=split_list[-1]
-		split_list.pop()
-		split_list.append("revcomp")
-		for i in split_list:
-			output_str=output_str + i + "/"
-		output_str+=filename
-		output_str = output_str[:-4] + "_revComp.fq"
-               	
-		#run seqtk
-		out_file = open(output_str,"w")
+                split_list = line.split("/")
+                filename = split_list[-1]
+                split_list.pop()
+                split_list.append("revcomp")
+                for i in split_list:
+                    output_str = output_str + i + "/"
+                    output_str += filename
+                    output_str = output_str[:-4] + "_revComp.fq"
+
+                #run seqtk
+                out_file = open(output_str, "w")
                 cmd = seqtk_path + " " + line
                 ps = subprocess.Popen(cmd, shell=True, stdout=out_file)
                 ps.wait()
                 out_file.close()
                 files_processed += 1
-                print "Number of files processed:\t", files_processed
+                print("Number of files processed:\t", files_processed)
     file.close()
-
-
 
 
 parser = MyParser()
@@ -54,7 +54,6 @@ parser = MyParser()
 parser.add_argument(dest='inputFile', metavar='input_files', nargs=1,
                     help='File containing the list of files to process.')
 args = parser.parse_args()
-
 
 if __name__ == "__main__":
     revComp(args.inputFile)
---
+++ scaffolding/evaluation/extractContigs.py
@@ -0,0 +1,77 @@
+import argparse
+from itertools import islice
+import sys
+from collections import defaultdict
+from os.path import expanduser
+
+#This script extracts from a contigs/scaffolds file the ones longer than a given number
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('Error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+
+def processFastaFiles(inputFile, n, outputFile):
+
+        contig_seq = ""
+        contig_id = ""
+        keptcontigs,total_contigs = 0,0
+        discardedcontigs = -1 ##to account for the first line
+        with open(inputFile[0]) as file:
+            for line in file:
+
+                #print contig_seq
+                if line.startswith('>') and len(list(contig_seq)) >= n:
+
+                    with open(outputFile, "a") as out:
+                        out.write(contig_id + contig_seq)
+
+                    contig_id = line
+                    contig_seq = ""
+                    keptcontigs+=1
+                    total_contigs +=1
+
+                elif line.startswith('>') and len(list(contig_seq)) < n:
+                    contig_id = line
+                    contig_seq = ""
+                    discardedcontigs+=1
+                    total_contigs +=1
+
+                else:
+
+                    contig_seq += line
+
+
+
+            ###process last contig (after last >) ###
+            if len(list(contig_seq)) >= n:
+                with open(outputFile, "a") as out:
+                        out.write(contig_id + contig_seq)
+                keptcontigs+=1
+            else:
+
+                discardedcontigs+=1
+
+        print("\nNumber of contigs in file:\t", total_contigs)
+        print("Number of contigs longer than", n , ":\t", keptcontigs)
+        print("Number of contigs discarded:\t", discardedcontigs)
+        print("Percentage kept:", (float(keptcontigs) / total_contigs) * 100, "%")
+
+
+        file.close()
+        out.close()
+
+
+
+parser = MyParser()
+parser.add_argument(dest='inputFile', metavar='input_files', nargs=1,
+                    help='Contig Fasta file to be processed.')
+parser.add_argument('-n', '--contiglength', metavar='', type=int, required=True, help='Minimum length of contigs to extract')
+parser.add_argument('-o', '--outputFile', metavar='', required = True, help='Output file')
+args = parser.parse_args()
+
+
+processFastaFiles(args.inputFile,args.contiglength,args.outputFile)
+
