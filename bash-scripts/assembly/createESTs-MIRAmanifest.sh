4ccab15aa93b5200ffa50b876616f18110baa93f
pedro <psbpedrobarbosa@gmail.com>
2015-02-17 15:17:42 +0000

    mira ests final so far

--- bash-scripts/assembly/createESTs-MIRAmanifest.sh
+++ bash-scripts/assembly/createESTs-MIRAmanifest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-"[-HS:mnr] and [-HS:nrr] respectively [-HS:nrc]. I'll come back to [-SK:bph]"
 display_usage(){
- printf "Script to automatically generate the manifest file to input in Mira for an EST assembly projectts.\n
+ printf "Script to automatically generate the manifest file to input in Mira for EST assembly projectts.\n
  Usage:
     -1st argument must be the project name to use in Mira.The name will be the prefix for the manifest file generated.
     -2nd argument must be a file with the list of files to assemble.
     -3rd argument must be the number of threads to use [INT value].
     -4th argument must be the maximum ammount of memory to use [INT value].
     -5th argument must be the percentage of memory to keep free [0< INT <100}.
-    -6th argument is optional. If set to yes, Mira will force some steps to use less memory, with the cost in the runtime. [yes|no] Default:no.\n"
+    -6th argument is optional. If set to yes, Mira will force some steps to use less memory, with the cost in the runtime. [yes|no] Default:no.
+    -7th argument is optional. If set to yes, Mira will execute the clipping algorithm on the reads along with the Poly-A tail removal. [yes|no] Default: yes.\n\n"
 }
 #You should always use the resume option ('-r') when calling Mira. It will resume the assembly at the point where some special files were written.\n
 
 #################Check if required arguments were provided########
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ] || [ -z "$8" ] ; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] ; then
     printf "Please provide the required arguments for the script.\n\n"
     display_usage
     exit 1
@@ -28,7 +28,14 @@ OUTPUT_FILE="${PROJECT_NAME}-manifest.txt"
 #454 files to assemble.
 while read line
 do
-ESTs_454_DATA="$ESTs_454_DATA $line"
+    if [ -f "$line" ] ; then
+        ESTs_454_DATA="$ESTs_454_DATA $line"
+
+    else
+        printf "File $line does not exist. Please check this out.\n"
+        display_usage
+        exit 1
+    fi
 done < "$2"
 
 
@@ -40,8 +47,8 @@ function read_groups(){
 
 
 ###454 ESTs ########
-if [ -d "$ESTs_454_DATA" ] ; then
-    cat <<EOF >> $OUTPUT_FILE
+
+cat <<EOF >> $OUTPUT_FILE
 
 #ESTs reads group
 readgroup = 454-ESTs-data
@@ -49,21 +56,15 @@ data = ${ESTs_454_DATA}
 technology = 454
 EOF
 
-else
-    printf "File regarding the path for the 454 fasta files is not valid. Change the '$ESTs_454_DATA' variable in
-        the script to a valid path.\n"
-            display_usage
-            exit 1
-fi
-
 }
 
+
 function settings(){
+
+
 ####PARAMETERS###
-
-
-
 #Min reads per contigs [default 2]. Should i put one to get very low abundant transcripts? test it later : −AS:mrpc = 1
+#Is poly tails removed by seqclean ? If so, disable this in Mira
 #Repeat mask parameters. [-HS:mnr] and [-HS:nrr] respectively [-HS:nrc]. I'll come back to [-SK:bph]"
 
 if [ -z "$4" ] || [ "$4" = "no" ]; then
@@ -71,19 +72,31 @@ if [ -z "$4" ] || [ "$4" = "no" ]; then
 cat <<EOF >> $OUTPUT_FILE
 
 #PARAMETERS
-parameters = COMMON_SETTINGS -GE:not=$1:amm=no:mps=$2:kpmf=$3 -NW:cmrnl=warn -SK:not=$1 \
-
+parameters = COMMON_SETTINGS -GE:not=$1:amm=no:mps=$2:kpmf=$3 -NW:cmrnl=warn -SK:not=$1 \\
 EOF
 
 #add parameters to force memory reduction
 else
+    print "Forcing Mira to use less memory.\n"
 cat <<EOF >> $OUTPUT_FILE
 
 #PARAMETERS
-parameters = COMMON_SETTINGS -GE:not=$1:amm=no:mps=$2:kpmf=$3 -NW:cmrnl=warn -SK:not=$1:mhpr=500:mhim=10000000 \
+parameters = COMMON_SETTINGS -GE:not=$1:amm=no:mps=$2:kpmf=$3 -NW:cmrnl=warn -SK:not=$1:mhpr=500:mhim=10000000 \\
+EOF
+fi
+
+##clipping algorithm [disable both poly-A tail removal step and the proposed end clipping algorithm]
+if [ -z "$5" ] || [ "$5" = "yes" ]; then
+    printf "Clipping will be performed as default.\n"
+else
+    printf "Clipping will not be performed.\n"
+cat <<EOF >> $OUTPUT_FILE
+454_SETTINGS -CL:pec=no:cpat=no
 
 EOF
 fi
+
+
 }
 
 
@@ -109,12 +122,14 @@ EOF
 read_groups
 
 ###PARAMETERS
-if [ -z "$6" ]; then
-    settings $4 $5 $6
-elif [ "$6" = "yes" ] || [ "$6" = "no" ]; then
-    settings $6 $7 $8 $6
+if [ -z "$6" ] && [ -z "$7" ]; then
+    settings $3 $4 $5
+elif [ "$6" = "yes" ] || [ "$6" = "no" ] && [ -z "$7" ]; then
+    settings $3 $4 $5 $6
+elif [ "$6" = "yes" ] || [ "$6" = "no" ] && [ "$7" = "yes" ] || [ "$7" = "no" ] ; then
+    settings $3 $4 $5 $6 $7
 else
-    printf "\nPlease provide a rigth value for the 9th parameter.\n\n"
+    printf "\nPlease provide a rigth value for the 6th or 7th parameter.\n\n"
     display_usage
     exit 1
 fi
