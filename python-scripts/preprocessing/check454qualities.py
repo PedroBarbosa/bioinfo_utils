3f7fd3bcfb2593c8b74219ebc36bd8e1329cbb13
pedro <psbpedrobarbosa@gmail.com>
2015-03-03 14:38:45 +0000

    454 script bug fix

--- python-scripts/preprocessing/check454qualities.py
+++ python-scripts/preprocessing/check454qualities.py
@@ -34,23 +34,23 @@ def write2file(dict, numb_nuc, numb_reads, outputfile):
         os.remove(outputfile)
     with open(outputfile, "w") as csvfile:
         writer = csv.writer(csvfile,dialect=csv.excel_tab)
-        writer.writerow('Total number of reads:', numb_reads)
-        writer.writerow('Total number of nucleotides:', numb_nuc)
-        writer.writerow()
-        writer.writerow('Phread quality score', 'Number of times', 'Percentage (%)')
+        writer.writerow(('Total number of reads:', numb_reads))
+        writer.writerow(('Total number of nucleotides:', numb_nuc))
+        writer.writerow('')
+        writer.writerow(('Phread quality score', 'Number of times', 'Percentage (%)'))
 
 
         od = collections.OrderedDict(sorted(dict.items()))
 
         for k, v in od.iteritems():
-            percentage = round((float(v / numb_nuc) * 100), 3)
-            writer.writerow(k,v,percentage)
-            
+            percentage = round((v / float(numb_nuc) * 100), 3)
+            writer.writerow((k,v,percentage))
+
         csvfile.close()
 
 
 parser = argparse.ArgumentParser(description='Script to simply analyze the qualities of a 454 file.')
-parser.add_argument(dest='qualities_file', metavar='454 qualities file', nargs=1,
+parser.add_argument(dest='qualities_file', metavar='454_qualities_file', nargs=1,
             help='File with the quality values in Contigs/Scaffolds fasta file to be processed.')
 parser.add_argument(dest='output_file', metavar='output_file', nargs=1, help='Output file')
 args = parser.parse_args()
@@ -59,5 +59,6 @@ args = parser.parse_args()
 if __name__ == "__main__":
     print("Checking qualities of the reads..")
     dict, numb_nuc, numb_reads = process_qual_file(args.qualities_file)
-    print("Writing the output to ", args.output_file )
-    write2file(dict, numb_nuc, numb_reads, args.output_file)
+    print("Writing the output to " + args.output_file[0] + "..")
+    write2file(dict, numb_nuc, numb_reads, args.output_file[0])
+    print("Done.")
