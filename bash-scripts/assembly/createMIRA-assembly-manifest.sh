66880c866497666ced39e24652c4ccbb8f00b62c
pedro <psbpedrobarbosa@gmail.com>
2015-06-05 17:25:54 +0100

    add code to support ion torrent data on the generation of the manifest file

--- bash-scripts/assembly/createMIRA-assembly-manifest.sh
+++ bash-scripts/assembly/createMIRA-assembly-manifest.sh
@@ -12,7 +12,9 @@ display_usage(){
     -6th argument must be the number of threads to use [INT value].
     -7th argument must be the maximum ammount of memory to use [INT value].
     -8th argument must be the percentage of memory to keep free [0< INT <100].
-    -9th argument is optional. If set to yes, Mira will force some steps to use less memory, with the cost in the runtime. [yes|no] Default:no.\n"
+    -9th argument is optional. If set to true, Mira will force some steps to use less memory, with the cost in the runtime. [true|false] Default:false.
+    -10th argument is optional. If set to true, Mira will use IonTorrent reads to perform assembly. [true|false] Default:false.\n"
+
 }
 #You should always use the resume option ('-r') when calling Mira. It will resume the assembly at the point where some special files were written.\n
 
@@ -31,6 +33,9 @@ OUTPUT_FILE="${PROJECT_NAME}-manifest.txt"
 
 #Path to the genomic 454 sequencing FASTA files - All files of the directory will be used so we can provide a path in Mira
 GENOMIC_454_PATH="/mnt/msa/BIOCANT/genomic-data/SFF_genom/FASTQ_RAW/"
+
+#Path to the genomic IonTOrrent sequencing FASTQ file
+ION_TORRENT_PATH="/home/pedro/Desktop"
 
 #Path to the file that lists Illumina files wit the threshold Q20L80/Q20L40
 #LIST_ILLUM_PE_MP_PATH="/mnt/msa/workflow_scripts/LIST_FILES/listFiles_Q20L80-PE_Q20L20-MP.txt"
@@ -218,8 +223,7 @@ EOF
         fi
 
     else
-        printf "File regarding the list of illumina pairs is not valid. Change the '$LIST_ILLUM_PE_MP_PATH' variable in
-        the script to a valid file.\n"
+        printf "String regarding the list of illumina pairs is not valid. Change the '$LIST_ILLUM_PE_MP_PATH' variable in the script to a valid file.\n\n"
         display_usage
         exit 1
     fi
@@ -240,14 +244,37 @@ data = ${GENOMIC_454_PATH}
 technology = 454
 EOF
         else
-            printf "File regarding the path for the 454 fasta files is not valid. Change the '$GENOMIC_454_PATH' variable in
-        the script to a valid path.\n"
+            printf "String regarding the path for the 454 fastq files is not valid. Change the '$GENOMIC_454_PATH' variable in the script to a valid path.\n\n"
             display_usage
             exit 1
         fi
 fi
 
+
+##### ION TORRENT ###########
+if [ "$6" = "true" ] && [ -d "$ION_TORRENT_PATH" ]; then
+
+    if [ -d "$ION_TORRENT_PATH" ] ; then
+        cat <<EOF >> $OUTPUT_FILE
+
+#Ion torrent read group
+readgroup = ion-torrent-data
+data = ${ION_TORRENT_PATH}
+technology = iontor
+EOF
+    else
+        printf "String regarding the path for the IonTorrent fastq files is not valid. Change the '$ION_TORRENT_PATH' variable in the script to a valid path.\n\n"
+            display_usage
+            exit 1
+    fi
+fi
+
+
+
 }
+
+
+
 
 function settings(){
 ####PARAMETERS###
@@ -255,15 +282,20 @@ function settings(){
 cat <<EOF >> $OUTPUT_FILE
 
 #PARAMETERS
-parameters = COMMON_SETTINGS -GE:not=$1:amm=no:mps=$2:kpmf=$3 -NW:cmrnl=warn \   
+parameters = COMMON_SETTINGS -GE:not=$1:amm=no:mps=$2:kpmf=$3 -NW:cmrnl=warn \
+
 EOF
 
 #add parameters to force memory reduction
-if [ -z "$4" ] || [ "$4" = "no" ]; then
+if [ -z "$4" ] || [ "$4" = "false" ]; then
     printf "Not forcing Mira to use less memory.\n"
+cat <<EOF >> $OUTPUT_FILE
+-SK:not=$1
+EOF
+
 else
 cat <<EOF >> $OUTPUT_FILE
--SK:mhpr=500:mhim=10000000
+-SK:not=$1:mhpr=500:mhim=10000000
 EOF
 fi
 }
@@ -288,11 +320,20 @@ EOF
 
 
 ####READ GROUPS
-read_groups $1 $2 $3 $4 $5
+if [ -z "${10}" ]; then
+    read_groups $1 $2 $3 $4 $5
+elif [ "${10}" = "true" ] || [ [ "${10}" = "false" ]; then
+    read_groups $1 $2 $3 $4 $5 "${10}"
+else
+    printf "\nPlease provide a rigth value for the 10th parameter.\n\n"
+    display_usage
+    exit 1
+fi
+
 ###PARAMETERS
 if [ -z "$9" ]; then
     settings $6 $7 $8
-elif [ "$9" = "yes" ] || [ "$9" = "no" ]; then
+elif [ "$9" = "true" ] || [ "$9" = "false" ]; then
     settings $6 $7 $8 $9
 else
     printf "\nPlease provide a rigth value for the 9th parameter.\n\n"
