20adef894d6dafefb09882a20b603792f6cf1a34
pedro <psbpedrobarbosa@gmail.com>
2015-09-30 17:01:33 +0100

    updated tophat2 script with more parameters

--- bash-scripts/read-mapping/createTopHat2command.sh
+++ bash-scripts/read-mapping/createTopHat2command.sh
@@ -5,12 +5,14 @@ Usage:
     -1st argument must be the file with the RNA seq reads to map.
     -2nd argument must be the reference indexed database.
     -3rd argument must be the number of threads to use in the process.
-    -4th argument is optional. If enabled, it refers to coverage based search for junctions. Useful for more sensitivity. Available options [true|false].
-    -5th argument is optional. If enabled, only the read alignments where both reads in a pair are mapped, are reported. Available options [true|false].\n"
+    -4th argument is optional. If enabled, it refers to coverage based search for junctions. Useful for more sensitivity. Available options [true|false] Default: false.
+    -5th argument is optional. If enabled, only the read alignments where both reads in a pair are mapped, are reported. Available options [true|false] Default: false.
+    -6th argument is optional. If enabled, you select the RNA seq protocol used (Strand specific vs Unstranded). Available options [RF|FR|unstranded] Default: unstranded.
+    -7th argument is optional. If enabled, you are able to add read group information to the output. Please write the readgroup id for this mapping [STRING].\n"
 } 
 
 
-#--no-discordant	For paired reads, report only concordant mappings. Check this options. Only pairs that rely on the insert size provided.
+
 exec="/opt/tools/tophat-2.1.0.Linux_x86_64/tophat2"
 
 #check if required arguments are there and display usage message
@@ -65,6 +67,7 @@ if [ -n "$4" ]; then
         command="$command --coverage-search"
     elif [ "$4" != "false" ] ; then
         printf "ERROR: Please provide a valid value for the 4th argument"
+        exit 1
     fi
 fi
 
@@ -73,7 +76,23 @@ if [ -n "$5" ]; then
         command="$command --no-mixed"
     elif [ "$5" != "false" ] ; then
         printf "ERROR: Please provide a valid value for the 5th argument"
+        exit 1
     fi
+fi
+
+if [ -n "$6" ]; then
+	if [ "$6" = "RF" ]  ; then
+        command="$command --library-type fr-firststrand"
+    elif [ "$6" = "FR" ] ; then
+    	command="$command --library-type fr-secondstrand"
+    elif [ "$6" != "unstranded" ]; then
+    	printf "ERROR: Please provide a valid value for the RNAseq strand specific protocol."
+    	exit 1
+    fi
+fi
+
+if [ -n "$7" ]; then
+    command="$command --rg-id $7 --rg-sample $7-sample --rg-library $7-$6"
 fi
 
 #check if output file already exists. If so, delete it
