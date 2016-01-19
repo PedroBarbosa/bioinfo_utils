082713c21a3b0bfd30423adb1d7c18d3198b4f00
Pedro Barbosa <psbpedrobarbosa@gmail.com>
2016-01-19 17:55:43 +0000

    some changes in fpkm analysis

--- bash-scripts/diffExp/cufflinks-suite/runFPKManalysisFromCufflinks.sh
+++ bash-scripts/diffExp/cufflinks-suite/runFPKManalysisFromCufflinks.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 if [ -z "$1" ] ; then
 	printf "Please add the a file with the list of files to process\n"
+	printf "This script requires gene or isoforms tracking files produced by cufflinks, not the GTF file\n"
 	exit 1
 fi
 
@@ -16,10 +17,12 @@ do
 	all_features=$(tail -n +2 $line | cut -f1,10 | sort | uniq |  wc -l)
 	expressed_features=$(tail -n +2 $line | cut -f1,10 | sort | uniq | cut -f2 | grep -cwv "0")  
 	duplicate_features=$(tail -n +2 $line | cut -f1,10 | cut -f1 |sort | uniq -cd | wc -l)
-tail -n +2 $line | cut -f1,10 | cut -f1 |sort | uniq -cd
+	featuresExpressedFromOriginalGTF=$(tail -n +2 $line | cut -f1,10 | grep -v "CUFF" | sort | uniq | cut -f2 | grep -cwv "0")
+#tail -n +2 $line | cut -f1,10 | cut -f1 |sort | uniq -cd
 	printf "###SAMPLE $sample_name###\n"
 	printf "Number of features tracked:\t$all_features\n"
 	printf "Number expressed features:\t$expressed_features\n"
+	printf "Number of features expressed mapping reference genes from original GTF:\t$featuresExpressedFromOriginalGTF\n"
 	printf "Number of unique features in file:\t$unique_features\n"
 	printf "Number of duplicate features in file:\t$duplicate_features\n"
 	printf "Duplicated features in file:\t%s\n\n\n" "$(tail -n +2 $line | cut -f1,10 | cut -f1 |sort | uniq -cd)"
