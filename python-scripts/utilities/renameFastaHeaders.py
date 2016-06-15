d3fb35a5c9ce6e5ba93b525e12dbc003736bde1b
gnsbr <genosuber.bioinfo@gmail.com>
2016-06-15 18:25:26 +0100

    rename fasta headers

---
+++ python-scripts/utilities/renameFastaHeaders.py
@@ -0,0 +1,34 @@
+from Bio import SeqIO
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Script to rename the header of a FASTA file, either by adding string in the end of header or totally replace it. Biopython needed')
+parser.add_argument(dest='fastaFile', metavar='fastaFile', help='FASTA file to process.')
+parser.add_argument('-a', metavar='add', help='String to add to the end of each header. String will be concatenated to each header with no spaces.')
+parser.add_argument('-r', metavar='replace', help='Base string to replace all headers completely. For each record a "_i" number will be incremented.')
+args = parser.parse_args()
+
+if args.a and args.r:
+    raise ValueError("Choose only one of the options: add [-a] or replace [-r]")
+
+elif args.a:
+
+    handle = open(args.fastaFile,"rU")
+
+    for seq_record in SeqIO.parse(handle,"fasta"):
+        old_header = seq_record.id
+        seq_record.id = old_header + args.a
+        print('>' + seq_record.id + '\n' + seq_record.seq)
+    handle.close()
+
+elif args.r:
+    i=1
+    handle = open(args.fastaFile,"rU")
+    for seq_record in SeqIO.parse(handle,"fasta"):
+        seq_record.id = args.r + '_' + str(i)
+        print('>' + seq_record.id + '\n' + seq_record.seq)
+        i+=1
+    handle.close()
+
+else:
+    raise ValueError("Please set one of the options: add [-a] or replace [-r]")
