c155cf30e322b5146d439358400c2c3e7b71b45a
gnsbr <genosuber.bioinfo@gmail.com>
2016-06-24 16:48:18 +0100

    eggnoc annot

---
+++ python-scripts/functional-annotation/addDescriptionToEggNOGrecords.py
@@ -0,0 +1,103 @@
+import argparse
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(message)s')
+import urllib.request
+import json
+from collections import defaultdict
+
+
+def nogs2list(nogsFile):
+    listnogs=[]
+    with open(nogsFile, 'r') as file:
+        for nog in file:
+            if not nog in listnogs:
+                listnogs.append(nog.rstrip())
+            else:
+                logging.warning("%s id repeated in file" % nog)
+    return listnogs
+
+
+def getAnnotationFromAPI(listNOGs,output):
+    #http://eggnogapi.embl.de/nog_data/[dataformat]/[attributes]/[nogname]
+
+    #[dataformat] currently supported data formats are:
+    #    json: data is returned in compressed JSON format (allows for multiple attributes)
+    #    text: data is returned as a text string
+    #    file: data is returnend as text file or as tar.gz (allows for multiple attributes)
+    #[attributes] One or more (comma separated):
+    #    fasta, raw_alg, trimmed_alg, tree, go_terms, domains
+    #[nogname] A valid EggNOG group name (i.e. ENOG410ZSWV or COG0575)
+
+    lessFreqDom = 0
+    out_dict = defaultdict()
+
+
+    i=1
+    for nog in listNOGs:
+        logging.info("Processing %s NOG.. %i" % (nog,i))
+        i+=1
+        f = urllib.request.urlopen('http://eggnogapi.embl.de/nog_data/json/domains,go_terms/'+nog).read()
+        data = json.loads(f.decode('utf-8'))
+       # print(data)
+       # print("\n\n\n\n")
+        for k,subdict in iter(data.items()):
+            out_dict[nog] = []
+            if k == "go_terms":
+
+                for go_category, gosListPerCategory in iter(subdict.items()):
+                    go_value = ""
+                    for go in gosListPerCategory:
+                        if float(go[4]) > 90: #if frequency of this GO in the sequences used to produced this NOG is higher than 90%
+                            go_value = go_value + go[0] + "[" + go[1] + "]" + ";"
+                    if go_value == "":
+                        out_dict[nog].append("-")
+                    else:
+                        out_dict[nog].append(go_value)
+                        print(out_dict[nog][2])
+
+                 #print(k,subdict)
+                 #print("\n\n\n")
+    #         if k == "domains":
+    #             for subkey,value in iter(data.items()):
+    #                 if subkey != "dom_header":
+    #
+    #                     oneHighFreqDomain = False
+    #                     out_dict[nog] = []
+    #                     for domain_database,listDomainsPerDb in iter(value.items()):
+    #                         domain_value = domain_database
+    #                         for domain in listDomainsPerDb:
+    #                             if float(domain[2]) > 90: #if frequency of this domain in the sequences used to produced this NOG is higher than 90%
+    #                                 domain_value = domain_value + "**" + domain[0]
+    #                                 oneHighFreqDomain = True
+    #                             else:
+    #                                 lessFreqDom += 1
+    #                         if oneHighFreqDomain:
+    #                             out_dict[nog].append(domain_value)
+    #
+    #                     for nog,domains in iter(out_dict.items()):
+    #                         if len(domains) == 0:
+    #                             out_dict[nog].extend(["No SMART domains", "No PFAM domains"])
+    #                         elif len(domains) == 1 and any("PFAM" in s for s in domains):
+    #                             domains.insert(0,"No SMART domains")
+    #                             out_dict[nog] = domains
+    #                         elif len(domains) == 1 and any("SMART" in s for s in domains):
+    #                             out_dict[nog].append("No PFAM domains")
+    #
+    # for k,v in iter(out_dict.items()):
+    #     print(k,v)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Script add annotation information of an eggNOG record. Uses eggNOG API to retrieve data. Outputs a tab separarted file.')
+    parser.add_argument(dest='nogs', metavar='nogsFile', help='File listing the NOGs to process. One perl line.')
+    parser.add_argument(dest='output', metavar='outputFile', help='Output File')
+    parser.add_argument('-a',metavar ='-addNOGAnnotationFile', help='Additional file with information about each NOG, particularly COG categories and descriptions. Downloadable from eggNOG website.')
+    args = parser.parse_args()
+
+
+    listNOGs = nogs2list(args.nogs)
+    getAnnotationFromAPI(listNOGs, args.output)
+
+if __name__ == "__main__":
+    main()
