676f2417565ea935bafea7783fff2ea016cfb875
gnsbr <genosuber.bioinfo@gmail.com>
2016-08-23 11:38:05 +0100

    add eggnog support to assign annotation to differential expression features

--- python-scripts/diffExp/assignAnnotationToDiffExprFeatures.py
+++ python-scripts/diffExp/assignAnnotationToDiffExprFeatures.py
@@ -15,15 +15,18 @@ def processFeaturesIDs(listDiffExpressed,onlyIds ):
     logging.info("Processing " + listDiffExpressed + " file..")
     logging.info("Creating dictionary of differential expressed features..")
     with open(listDiffExpressed) as file:
-
         if onlyIds:
             for line in file:
-                if line.rstrip() in dict:
+                line.rstrip()
+                if line.startswith("#"):
+                    commented_lines.append(line)
+                elif line.rstrip() in dict:
                     logging.error("Error Duplicate feature ID in" + listDiffExpressed + " file:\t" + line.rstrip())
                     exit(1)
                 else:
                     dict[line.rstrip()] = []
                     total_feature += 1
+
         else:
             for line in file:
                 line.rstrip()
@@ -93,6 +96,20 @@ def processAnnotationFile(annotationFile, database_searched,dict_features, total
                             dict_features[query] = new_list
                             previous_query = query
 
+        elif database_searched == "eggnog":
+            for line in file:
+                line.rstrip()
+                if not line.startswith('#') :
+                    query = line.split()[2]
+                    if query != previous_query:
+                        annotated_features += 1
+                        eggnong_id = line.split("\t")[0].split(".")[1] #eggnog ortholog group in an hmmscan hits file
+                        if noDescription:
+                            description = "No description available"
+                        else:
+                            logging.error("When searching against eggnog (using hmmer), no description is available in the table output format (generated used "
+                                          "tblout or domtblout or pfamtblout arguments on hmmscan. Please set the '-n' argument and rerun the script without descriptions.")
+                            exit(1)
     file.close()
     if annotated_features == 0:
         logging.error("No features found in the annotation file are present in the list of differential expressed genes. Please check if the feature IDs are concordant.")
@@ -130,8 +147,9 @@ def main():
                                                 ' All non-feature lines must start with a "#".')
     parser.add_argument(dest='annotationFile', metavar='annotationFile', nargs=1, help='Blast like tab separated file with functional annotations.')
     parser.add_argument(dest='output_file', metavar='output_file', nargs=1, help='File to write the output.')
-    parser.add_argument(dest='database_searched', metavar='database_searched', nargs=1, type=str, choices=['swissprot','ncbi-nr'],help='Database used for similarity searches. If ncbi-nr is selected, ncbi id must come on the 2nd column and its decription on the 3rd (See "-n" argument for exceptions).Available choices [swissprot, ncbi-nr]')
-    parser.add_argument(dest='software_used', metavar='software_used', nargs=1, type=str, choices=['rapsearch2', 'blastp'],help='Tool used to run similarity searches. Available choices [rapsearch2, blastp]. When blastp used, it is assumed that the description comes in the 3rd columns of the annotation file. If not, set "-n" argument\
+    parser.add_argument(dest='database_searched', metavar='database_searched', nargs=1, type=str, choices=['swissprot','ncbi-nr', 'eggnog'],help='Database used for similarity searches. If ncbi-nr is selected, ncbi id must come on the 2nd column and its decription on the 3rd (See "-n" argument for exceptions). If eggnog choosen, it is supposed that '
+        'hmmscan was run over hidden markov models representing the eggnog records. Available choices [swissprot, ncbi-nr, eggnog]')
+    parser.add_argument(dest='software_used', metavar='software_used', nargs=1, type=str, choices=['rapsearch2', 'blastp','hmmer'],help='Tool used to run similarity searches. Available choices [rapsearch2, blastp]. When blastp used, it is assumed that the description comes in the 3rd columns of the annotation file. If not, set "-n" argument\
     to process only the IDs of the hits.')
     parser.add_argument('-id', '--idOnly', action='store_true', help='If set, listDiffExpressed represents file with only a feature ID per line, rather than a multi column file from EdgeR.')
     parser.add_argument('-n', '--noDescriptionBlastp', action='store_true', help='By default, blastp does not output database records description. If blastp was not run with special configuration to report this information in the\
