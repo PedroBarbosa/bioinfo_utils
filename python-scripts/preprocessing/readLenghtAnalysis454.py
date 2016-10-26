979eeb171d90ae54873a4f0a7eff40052f74eec4
gnsbr <genosuber.bioinfo@gmail.com>
2016-10-26 17:21:02 +0100

    add N analysis to read length 454 script

--- python-scripts/preprocessing/readLenghtAnalysis454.py
+++ python-scripts/preprocessing/readLenghtAnalysis454.py
@@ -1,4 +1,6 @@
 import argparse
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from collections import Counter
 from operator import itemgetter
@@ -13,31 +15,38 @@ def processFastaFiles(inputFile):
 
     list_tuples = []
     lenghts = []
+    Ns = []
     handle = open(inputFile,'rU')
     sequences = SeqIO.parse(handle,'fasta')
     for record in sequences:
-        list_tuples.append((record.id, len(record.seq)))
+        upper=record.seq.upper()
+        countN = upper.count("N")
+        Ns.append(countN)
+        list_tuples.append((record.id, len(record.seq), countN))
         lenghts.append(len(record.seq))
 
     handle.close()
-    return  list_tuples, lenghts
+    return  list_tuples, lenghts, Ns
 
 def writeIndividualReadLenght(tuples, inputfile):
     basename = os.path.splitext(inputfile)[0]
     outFile = basename + "-indivReadLength.txt"
+    dictNsPerReadLenght = {}
     with open(outFile,'w') as out:
-        out.write("#readID\tlength\n")
-        for read, length in sorted(tuples,key=itemgetter(1),reverse=True):
-            out.write("%s\t%i\n" % (read,length))
+        out.write("#readID\tlength\tNs\n")
+        for read, length, nCounts in sorted(tuples,key=itemgetter(1),reverse=True):
+            out.write("%s\t%i\t%i\n" % (read,length,nCounts))
+            dictNsPerReadLenght[length] = nCounts
     out.close()
+    return dictNsPerReadLenght
 
-
-def countsAndStats(lengths, inputfile):
+def countsAndStats(lengths, listNs ,inputfile):
     basename = os.path.splitext(inputfile)[0]
     outFile = basename + "-statsAndCounts.txt"
     dict_counter = Counter(lengths)
 
     with open(outFile,'w') as out:
+        ####Read lengths####
         out.write("Total number of reads:\t%i\n" % len(lengths))
         out.write("Mean read length:\t%i\n" % np.mean(lengths))
         out.write("Median of read lengths:\t%i\n" % np.median(lengths))
@@ -48,6 +57,15 @@ def countsAndStats(lengths, inputfile):
         for val in most_common:
             out.write("%i\t%i\n" % (val[0],val[1]))
 
+
+        ####Ns#####
+        out.write("\n\nTotal number of Ns in reads:\t%i\n" % sum(listNs))
+        out.write("Number of reads with Ns:\t%i\n" % (len(listNs) - listNs.count(0)))
+        out.write("Mean number of Ns per read:\t%i\n" % np.mean(listNs))
+        out.write("Median of Ns per read:\t%i\n" % np.median(listNs))
+        out.write("Maximum number of Ns in one read:\t%i\n" % np.max(listNs))
+
+        ####Counts for histogram of read lengths#####
         out.write("\nValues for histogram counts:\n")
         for key in sorted(dict_counter.keys()):
             out.write("%i\t%i\n" % (key,dict_counter[key]))
@@ -56,15 +74,25 @@ def countsAndStats(lengths, inputfile):
 
 
 
-def drawHistogram(counter, inputfile):
+def drawHistograms(counter, dictNsReadLength, inputfile):
     basename = os.path.splitext(inputfile)[0]
-    outFile = basename + "-histogramReadLength.png"
+    outFileReadLength = basename + "-histReadLength.png"
+    outFileNsReadLength = basename + "-histNsPerReadLength.png"
 
+    ####Read length#####
     val, weight = zip(*[(k, v) for k,v in counter.items()])
-    plt.hist(val, bins= 15,weights=weight, range=[0,2000])
+    plt.hist(val, bins= 20,weights=weight, range=[0,2000])
     plt.xlabel('Read lenght bins')
     plt.ylabel('Counts')
-    plt.savefig(outFile)
+    plt.savefig(outFileReadLength)
+    plt.close()
+
+    ####Ns###########
+    val2, weight2 = zip(*[(k, v) for k,v in dictNsReadLength.items()])
+    plt.hist(val2, bins= 20,weights=weight2, range=[0,2000], color="green")
+    plt.xlabel('Read lenght bins')
+    plt.ylabel('Ns')
+    plt.savefig(outFileNsReadLength)
 
 
 parser = argparse.ArgumentParser(description='Script analyse the lenght of 454 reads. They must be converted to fasta. ')
@@ -73,7 +101,7 @@ args = parser.parse_args()
 
 for file in args.inputFile:
     logging.info("Processing %s file." % file)
-    list_tuples, alllenghts = processFastaFiles(file)
-    writeIndividualReadLenght(list_tuples,file)
-    counter4hist =countsAndStats(alllenghts,file)
-    drawHistogram(counter4hist,file)
+    list_tuples, alllenghts, listNs = processFastaFiles(file)
+    dict_NsReadLen = writeIndividualReadLenght(list_tuples,file)
+    counter4hist =countsAndStats(alllenghts,listNs,file)
+    drawHistograms(counter4hist,dict_NsReadLen,file)
