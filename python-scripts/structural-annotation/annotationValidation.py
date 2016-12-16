d6c2d1bf0e6b2a52a73935639059a5aa4f8f521f
gnsbr <genosuber.bioinfo@gmail.com>
2016-12-16 17:32:29 +0000

    validation done, only missing the charts

--- python-scripts/structural-annotation/annotationValidation.py
+++ python-scripts/structural-annotation/annotationValidation.py
@@ -1,7 +1,11 @@
 import argparse
 import logging
 import sys
+import os
+import numpy as np
 from collections import defaultdict
+from collections import OrderedDict
+from collections import Counter
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(message)s')
 
 
@@ -12,16 +16,17 @@ class validateAnnotation():
         self.genome_table = genome_table
         self.splitstring = splitstring
 
-        self.allpresentscaffolds = set()
-        self.genesFullyCovered = set()
-        self.setStrands = set()
+        self.scaffolds = set() #set of scaffolds with genes in the reference annotation
         self.genes = set() #set of genes in the reference annotation
         self.transcripts = set() # set of transcripts in the reference annotation
-        self.scaffolds = set() #set of scaffolds with genes in the reference annotation
+        self.allpresentscaffolds = set() #set of scaffolds with genes predicted in the set of assembled transcripts
         self.abundance = defaultdict(list)  # dictionary of all the transcripts TPM abundance estimation
         self.geneInterception = defaultdict(list) #dictionary of all present/absent genes
-        self.individualGeneMatchingRate = dict() #list representing average of accurate genes predicted
-        self.individualTranscriptMatchingRate = dict()  #list representing average of accurate transcripts predicted
+        self.individualGeneMatchingRate = OrderedDict() #list representing average of accurate genes predicted
+        self.individualTranscriptMatchingRate = OrderedDict()  #list representing average of accurate transcripts predicted
+        self.genesFullyCovered = set() #set of genes fully covered by RNA-seq reads (availalbe only when cov ref files are provided)
+        self.transcriptsFullyCovered = set() #set of trasncripts fully covered by RNA-seq reads (availalbe only when cov ref files are provided)
+        self.setStrands = set()
 
     def processReferenceAnnotGff(self,gff):
         logging.info("Processing reference annotation file..")
@@ -88,6 +93,34 @@ class validateAnnotation():
             logging.info("Done.")
             self.writeIndividualStats(gtf,indivscafollds,indivgenes,indivnewgenes,indivtranscript,indivnewtranscript)
 
+
+    def processCovRefs(self,covfile):
+        fully_cov_genes = set()
+        fully_cov_transcripts = set()
+        logging.info("Processing covRef file..")
+        crash=False
+        with open(covfile,'r') as infile:
+            for line in infile:
+                try:
+                    if not line.startswith("#") and line.split("\t")[2] == "mRNA":
+                        attrb = line.split("\t")[8]
+                        transcript = attrb.split(";")[0].split("=")[1]
+                        gene = attrb.split(";")[2].split("=")[1]
+                        fully_cov_genes.add(gene)
+                        fully_cov_transcripts.add(transcript)
+                        self.genesFullyCovered.add(gene)
+                        self.transcriptsFullyCovered.add(transcript)
+                except:
+                    logging.error("Problems parsing the coverage ref file %s" % covfile)
+                    logging.info("This step will be skipped.\n")
+                    crash=True
+                    break
+
+        infile.close()
+        if not crash:
+            print("Number of genes fully covered by RNA-seq reads in the %s transcriptome.\t%i" % (covfile.split(self.splitstring)[0], len(fully_cov_genes)))
+            print("Number of trancripts fully covered by RNA-seq reads in the %s transcriptome.\t%i" % (covfile.split(self.splitstring)[0], len(fully_cov_transcripts)))
+
     def writeIndividualStats(self,gtf,scaff,genes,newgenes,transcripts,newtranscripts):
         logging.info("Writing individual stats..")
         print("\n####################%s##############" % gtf.split(self.splitstring)[0])
@@ -107,7 +140,59 @@ class validateAnnotation():
         print("Total number of transcripts in reference\t%i" % len(self.transcripts))
         print("Total number of transcripts assembled in this specific transcriptome\t%i" % (len(transcripts) + len(newtranscripts)))
         print("Number of transcripts in the transcriptome matching the reference\t%i (%s%%)" % (len(transcripts),trans_frac_match))
-        print("Number of unknown new transcripts\t%i (%s%%)" % (len(newtranscripts),round(len(newtranscripts)/(len(transcripts) + len(newtranscripts))*100,2)))
+        print("Number of unknown new transcripts\t%i (%s%%)\n" % (len(newtranscripts),round(len(newtranscripts)/(len(transcripts) + len(newtranscripts))*100,2)))
+
+    def writeGlobal(self,outprefix):
+        with open(outprefix + '_overall-stats.txt', 'w') as outfile:
+            outfile.write("Scaffolds in the reference with genes predicted:\t%i\n" % len(self.scaffolds))
+            outfile.write("Scaffolds in the set of assembled transcriptomes with expressed transcripts:\t%i\n\n" % len(self.allpresentscaffolds))
+            outfile.write("Number of genes in the reference annotation\t%i\n" % len(self.genes))
+
+            overall_fraction_genes = round(len(self.geneInterception)/len(self.genes)*100,2)
+            overall_fraction_transcripts = round(len(self.abundance)/len(self.transcripts)*100,2)
+
+
+            outfile.write("Number of genes from the reference present in the set of assembled transcriptomes\t%i (%s%%)\n\n" % (len(self.geneInterception), overall_fraction_genes))
+            outfile.write("Number of transcripts in the reference annotation\t%i\n" % len(self.transcripts))
+            outfile.write("Number of transcripts from the reference present in the set of assembled transcriptomes\t%i (%s%%)\n\n" % (len(self.abundance), overall_fraction_transcripts))
+
+            if self.genesFullyCovered:
+                outfile.write("Number of genes from the reference fully covered by RNA-seq reads\t%i\n" % len(self.genesFullyCovered))
+                outfile.write("Number of transcripts from the reference fully covered by RNA-seq reads\t%i\n\n" % len(self.transcriptsFullyCovered))
+
+            maprate_g,maprate_t = [],[]
+            for k,v in self.individualGeneMatchingRate.items():
+                maprate_g.append(v)
+            for k,v in self.individualTranscriptMatchingRate.items():
+                maprate_t.append(v)
+
+
+            outfile.write("Per sample mean/median mapping rate at gene level\t%s/%s %%\n" % (round(np.mean(maprate_g),2), round(np.median(maprate_g),2)))
+            outfile.write("Per sample mean/median mapping rate at transcript level\t%s/%s %%\n\n" % (round(np.mean(maprate_t),2), round(np.median(maprate_t),2)))
+
+
+            counter = Counter(elem[1] for elem in self.setStrands)
+            outfile.write("Forward strand genes matching the reference\t%i\n" % counter["+"])
+            outfile.write("Reverse strand genes matching the reference\t%i\n\n" % counter["-"])
+
+        outfile.close()
+
+
+
+
+
+    def getGlobalStats(self):
+        logging.info("Writing global stats..")
+        if len(self.individualGeneMatchingRate) == 1:
+            logging.info("WARNING.Only one input file was provided, some analysis wont' be performed, and the global stats refer the same as the individual ones.")
+            self.writeGlobal(self.outputprefix)
+        else:
+            self.writeGlobal(self.outputprefix)
+
+
+
+def getNumberLines(file):
+    return sum(1 for line in open(file))
 
 def main():
 
@@ -134,38 +219,80 @@ def main():
             logging.error("CoverageRef files are only available when stringtie assemblies are produced. Either remove '-c' option or change '-s' to stringtie.")
             exit(1)
         else:
-            lines_gtfs = sum(1 for line in open(args.list))
-            lines_covRef = sum(1 for line in open(args.c))
+            lines_gtfs = getNumberLines(args.list)
+            lines_covRef = getNumberLines(args.c)
             if lines_gtfs != lines_covRef:
-                logging.error("Number of gtfs is different than coverageRef files.")
+                logging.error("Error. Number of gtfs is different than number of coverageRef files. Please check if you provided a coverage ref file in the '-c' argument, instead of a txt file listing those files, one perl line.")
                 exit(1)
 
             obj.processReferenceAnnotGff(args.r)
             with open(args.gtf_file, 'r') as listGtfs:
-                for line in listGtfs:
-                    obj.processFromStringtie(line, index, lines_gtfs)
+                with open(args.c, 'r') as covrefs:
+                    for line in listGtfs:
+                        covfile = ""
+                        for l in covrefs:
+                            if os.path.abspath(l).rsplit(obj.splitstring, 1)[0] == os.path.abspath(line).rsplit(obj.splitstring,1)[0]:
+                                covfile = l.rstrip()
+                                break
+                        if not covfile:
+                            logging.error("Error. File %s does not have a covref file asssociated. Gtfs and CovRef must be the same substring when split by sample identifier ('.', by default)." % line)
+                            exit(1)
+                        else:
+                            obj.processFromStringtie(line, index, lines_gtfs)
+                            obj.processCovRefs(covfile)
+
                     index += 1
+                covrefs.close()
             listGtfs.close()
+            obj.getGlobalStats()
 
     elif args.list:
         obj.processReferenceAnnotGff(args.r)
-        lines_gtfs = sum(1 for line in open(args.list))
+        lines_gtfs = getNumberLines(args.list)
         with open(args.gtf_file, 'r') as listGtfs:
             for line in listGtfs:
                 obj.processFromStringtie(line,index, lines_gtfs)
                 index += 1
-            listGtfs.close()
+        listGtfs.close()
+        obj.getGlobalStats()
 
+
+    elif args.c:
+
+        obj.processReferenceAnnotGff(args.r)
+        numbfiles = len(args.gtf_file)
+        lines_covRef = getNumberLines(args.c)
+        if numbfiles != lines_covRef:
+            logging.error("Error. Number of gtfs is different than number of coverageRef files. Please check if you provided a coverage ref file in the '-c' argument, instead of a txt file listing those files, one perl line.")
+            exit(1)
+
+        with open(args.c, 'r') as covrefs:
+            for file in args.gtf_file:
+                covfile = ""
+                for l in covrefs:
+                    if os.path.abspath(l).rsplit(obj.splitstring, 1)[0] == os.path.abspath(file).rsplit(obj.splitstring,1)[0]:
+                        covfile = l.rstrip()
+                        break
+                if not covfile:
+                    logging.error("Error. File %s does not have a covref file asssociated. Gtfs and CovRef must be the same substring when split by sample identifier ('.', by default)." % file)
+                    exit(1)
+                elif args.s == "stringtie":
+                    obj.processFromStringtie(file, index, numbfiles)
+                    obj.processCovRefs(covfile)
+                else:
+                    logging.error("Ref Coverage files are only available from stringtie assembly.")
+                    exit(1)
+                index += 1
+        covrefs.close()
+        obj.getGlobalStats()
 
     else:
-
         obj.processReferenceAnnotGff(args.r)
         numbfiles = len(args.gtf_file)
         for file in args.gtf_file:
             if args.s == "stringtie":
                 obj.processFromStringtie(file, index, numbfiles)
                 index += 1
-
-
+        obj.getGlobalStats()
 if __name__ == "__main__":
     main()
