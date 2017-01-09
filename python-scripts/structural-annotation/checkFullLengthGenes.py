59508ca1081255354b1681ea255c9f99b34c6cf4
gnsbr <genosuber.bioinfo@gmail.com>
2017-01-09 10:56:53 +0000

    small fixes

--- python-scripts/structural-annotation/checkFullLengthGenes.py
+++ python-scripts/structural-annotation/checkFullLengthGenes.py
@@ -16,7 +16,8 @@ def processGff(gff):
     firstStop=False
     existStart=True
     existStop=True
-    i=0
+    endstartUTRpred=False
+    endstopUTRpred=False
     with open(gff, 'r') as infile:
         previou_g_id = ""
         for line in infile:
@@ -33,7 +34,7 @@ def processGff(gff):
                             dict_utr[previou_g_id][1] = "no_stopCodon"
 
                         if dict_numberOfStartStop[previou_g_id][0] == 0 and dict_numberOfStartStop[previou_g_id][1] == 0:
-                            logging.info("IMPORTANT: %g gene does not have both start and stop codons")
+                            logging.info("IMPORTANT: %s gene does not have both start and stop codons" % previou_g_id)
                             notstartstop.append(previou_g_id)
 
                         elif dict_numberOfStartStop[previou_g_id][0] == 0:
@@ -60,7 +61,8 @@ def processGff(gff):
                     firstStop=True
                     existStart=False
                     existStop=False
-
+                    endstartUTRpred=False
+                    endstopUTRpred=False
 
                 elif "codon" in line.split("\t")[2]:
                     feature = line.split("\t")
@@ -75,36 +77,36 @@ def processGff(gff):
                         existStart=True
                         dict_numberOfStartStop[g_id][0] += 1
                         #forward
-                        if firstStart == True and dict_gene_coord_strand[g_id][2] == "+":
+                        if firstStart == True and dict_gene_coord_strand[g_id][2] == "+" and endstartUTRpred==False:
 
                             if int(feature[3]) == int(dict_gene_coord_strand[g_id][0]):
                                 dict_utr[g_id][0] = "no_5primeUTR"
-
+                                endstartUTRpred=True
                             elif int(feature[3]) > int(dict_gene_coord_strand[g_id][0]):
                                 length= int(feature[3]) - int(dict_gene_coord_strand[g_id][0])
                                 dict_utr[g_id][0] = "5primerUTR_" + str(length)
 
                             firstStart=False
 
-                        elif firstStart == False and dict_gene_coord_strand[g_id][2] == "+":
+                        elif firstStart == False and dict_gene_coord_strand[g_id][2] == "+" and endstartUTRpred==False:
 
                             logging.info("%s has multiple start codon.." % g_id)
                             if int(feature[3]) == int(dict_gene_coord_strand[g_id][0]):
                                 dict_utr[g_id][0] = "no_5primeUTR"
-
+                                endstartUTRpred=True
                         #reverse
-                        elif firstStart==True and dict_gene_coord_strand[g_id][2] == "-":
+                        elif firstStart==True and dict_gene_coord_strand[g_id][2] == "-" and endstartUTRpred==False:
 
                             if feature[4] == dict_gene_coord_strand[g_id][1]:
                                 dict_utr[g_id][0] = "no_5primeUTR"
                                 firstStart=False
-
+                                endstartUTRpred=True
                             elif int(feature[4]) < int(dict_gene_coord_strand[g_id][1]):
                                 length= int(dict_gene_coord_strand[g_id][1]) - int(feature[4])
                                 dict_utr[g_id][0] = "5primerUTR_" + str(length)
                                 firstStart=False
 
-                        elif firstStart==False and dict_gene_coord_strand[g_id][2] == "-":#when genes in negative strand we need to loo until last start codon appeared
+                        elif firstStart==False and dict_gene_coord_strand[g_id][2] == "-" and endstartUTRpred==False:#when genes in negative strand we need to loo until last start codon appeared
                             logging.info("%s has multiple start codon.." % g_id)
                             if int(feature[4]) < int(dict_gene_coord_strand[g_id][1]):
                                 length= int(dict_gene_coord_strand[g_id][1]) - int(feature[4])
@@ -112,45 +114,49 @@ def processGff(gff):
 
                             elif int(feature[4]) == int(dict_gene_coord_strand[g_id][1]):
                                 dict_utr[g_id][0] = "no_5primeUTR"
+                                endstartUTRpred=True
 
                     ###############################################
                     elif feature[2] == "stop_codon":
                         existStop=True
                         dict_numberOfStartStop[g_id][1] += 1
                         #forward
-                        if firstStop==True and dict_gene_coord_strand[g_id][2] == "+":
+                        if firstStop==True and dict_gene_coord_strand[g_id][2] == "+" and endstopUTRpred == False:
 
                             if int(feature[4]) == int(dict_gene_coord_strand[g_id][1]):
                                 dict_utr[g_id][1] = "no_3primeUTR"
-
+                                endstopUTRpred = True
                             elif int(feature[4]) < int(dict_gene_coord_strand[g_id][1]):
-                                length= int(dict_gene_coord_strand[g_id][0]) - int(feature[4])
+                                length= int(dict_gene_coord_strand[g_id][1]) - int(feature[4])
                                 dict_utr[g_id][1] = "3primerUTR_" + str(length)
                             firstStop=False
 
-                        elif firstStop==False and dict_gene_coord_strand[g_id][2] == "+":#when genes in positive strand we need to loop until last stop codon appears
+                        elif firstStop==False and dict_gene_coord_strand[g_id][2] == "+" and endstopUTRpred == False:#when genes in positive strand we need to loop until last stop codon appears
                             logging.info("%s has multiple stop codon.." % g_id)
                             if int(feature[4]) < int(dict_gene_coord_strand[g_id][1]):
                                 length= int(dict_gene_coord_strand[g_id][1]) - int(feature[4])
                                 dict_utr[g_id][1] = "3primerUTR_" + str(length)
                             elif int(feature[4]) == int(dict_gene_coord_strand[g_id][1]):
                                 dict_utr[g_id][1] = "no_3primeUTR"
+                                endstopUTRpred = True
                         #reverse
-                        elif firstStop==True and dict_gene_coord_strand[g_id][2] == "-":
+                        elif firstStop==True and dict_gene_coord_strand[g_id][2] == "-" and endstopUTRpred == False:
 
                             if feature[3] == dict_gene_coord_strand[g_id][0]:
                                 dict_utr[g_id][1] = "no_3primeUTR"
                                 firstStop=False
+                                endstopUTRpred=True
 
-                            elif int(feature[3]) > int(dict_gene_coord_strand[g_id][1]):
-                                length=  int(feature[3]) - int(dict_gene_coord_strand[g_id][1])
+                            elif int(feature[3]) > int(dict_gene_coord_strand[g_id][0]):
+                                length=  int(feature[3]) - int(dict_gene_coord_strand[g_id][0])
                                 dict_utr[g_id][1] = "3primerUTR_" + str(length)
                                 firstStart=False
 
-                        elif firstStop == False and dict_gene_coord_strand[g_id][2] == "-":
+                        elif firstStop == False and dict_gene_coord_strand[g_id][2] == "-" and endstopUTRpred == False:
                             logging.info("%s has multiple stop codon.." % g_id)
                             if feature[3] == dict_gene_coord_strand[g_id][0]:
                                 dict_utr[g_id][1] = "no_3primeUTR"
+                                endstopUTRpred = True
 
                 elif line.split("\t")[2] == "transcript":
 
@@ -184,7 +190,8 @@ def processGff(gff):
                 several.append(previou_g_id)
 
 
-        logging.info("\n################################\nNumber of genes without start codon\t%i" %len(nostart))
+        logging.info("\n################################\n")
+        logging.info("Number of genes without start codon\t%i" %len(nostart))
         logging.info("Number of genes without stop codon\t%i" %len(nostop))
         logging.info("Number of genes without start and stop codon\t%i\n" %len(notstartstop))
         logging.info("Number of genes with exactly one start and one stop codon\t%i" %len(oneone))
@@ -209,28 +216,33 @@ def processGff(gff):
                 noStop+=1
             else:
                 utr3prime+=1
-                #print(k)
 
-        logging.info("Number of genes with 5 prime UTR\t%i" % utr5prime)
-        logging.info("Number of genes with 3 prime UTR\t%i" % utr3prime)
+        logging.info("Number of genes with 5 prime UTR (the existing start codons from these genes are distinct from the begin coordinate of it. Start codons of some transcripts of gene may be missing).\t%i" % utr5prime)
+        logging.info("Number of genes with 3 prime UTR (the existing stop codons from these genes are distinct from the end coordinate of it. Stop codons of some transcripts of gene may be missing).\t%i" % utr3prime)
         logging.info("Genes with no 5 prime\t%i" % noutr5)
         logging.info("Genes with no 3 prime\t%i" % noutr3)
         logging.info("Genes with unknown 5prime UTR prediction (no start codon)\t%i" % noStart)
-        logging.info("Genes with unknown 3prime UTR prediction (no stop codon)\t%i" % noStop)
+        logging.info("Genes with unknown 3prime UTR prediction (no stop codon)\t%i\n" % noStop)
 
+        logging.info("Writing general output file..")
         with open("gene_information.txt", 'w') as outfile:
-            outfile.write("#gene_id\tstart_codon\tstop_codon\tnumber_start\tnumber_stop\tnumber_transcripts\ttranscripts_name\n")
+            outfile.write("#gene_id\tstart_codon\tstop_codon\tnumber_start\tnumber_stop\tnumber_transcripts\ttranscripts_name\t5primeUTR\t3primeUTR\n")
             for k,v in sorted(dict_gene_transcript.items(), key = lambda item : len(item[1]), reverse=True):
                 start=["no" if k in nostart or k in notstartstop else "yes"]
                 stop=["no" if k in nostop or k in notstartstop else "yes"]
+                utr5=["no" if "no_" in dict_utr[k][0] else "yes_" + dict_utr[k][0].split("_")[1]]
+                utr3=["no" if "no_" in dict_utr[k][1] else "yes_" + dict_utr[k][1].split("_")[1]]
                 outfile.write(k + "\t" + start[0] + "\t" + stop[0] + "\t" + str(dict_numberOfStartStop[k][0]) + "\t" + str(dict_numberOfStartStop[k][1])
-                              + "\t" + str(len(v)) + "\t" + ';'.join(v) + "\n")
+                              + "\t" + str(len(v)) + "\t" + ';'.join(v) + "\t" + utr5[0] + "\t" + utr3[0] + "\n")
 
-        for k,v in dict_gene_transcript.items():
-            i+=len(dict_gene_transcript[k])
-        #print(i)
-        Count = collections.Counter([len(v) for k,v in dict_gene_transcript.items()])
-        print(Count)
+        outfile.close()
+
+        logging.info("Writing potential genes to remove from annotation (no stop, no start, not stop and start)..")
+        with open("incomplete_genes.txt", "w") as outfile2:
+            outfile2.write("\n".join(notstartstop) + "\n")
+            outfile2.write("\n".join(nostart) + "\n")
+            outfile2.write("\n".join(nostop) + "\n")
+
 def main():
     parser = argparse.ArgumentParser(description='Script to look for start, stop and UTR region in gff3 files.')
     parser.add_argument(dest='gff_file', metavar='gff', help='Annotation file to be analyzed.')
