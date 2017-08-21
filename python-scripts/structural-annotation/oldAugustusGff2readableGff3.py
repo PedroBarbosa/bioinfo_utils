4c987231ffdb74163b1b6f582942f82b53d5242b
gnsbr <genosuber.bioinfo@gmail.com>
2017-08-21 12:14:42 +0100

    add sequence regions to the output

--- python-scripts/structural-annotation/oldAugustusGff2readableGff3.py
+++ python-scripts/structural-annotation/oldAugustusGff2readableGff3.py
@@ -5,11 +5,26 @@ import collections
 import operator
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(message)s')
 
+def createGenomeTableDict(refGenomeTable):
+    gnmTbl={}
+    with open(refGenomeTable, 'r') as infile:
+        for line in infile:
+            if len(line.split("\t")) != 2:
+                print("Error, genome table files must have exactly 2 columns.")
+                exit(1)
+            elif line.split("\t")[0] in gnmTbl:
+                print("Error, repeated scaffold id in genome table file %s" % refGenomeTable)
+            else:
+                gnmTbl[line.split("\t")[0]] = line.split("\t")[1].rstrip()
+        #print("%i reference sequences processed" % len(gnmTbl))
+    infile.close()
+    return dict
 
-def readAugustusGff(infile,outfile):
+
+def readAugustusGff(infile,outfile,dict):
     with open(infile,'r') as infl:
         with open(outfile,'w') as outfl:
-            outfl.write("#Output generated with Pedro script to convert IDs nomenclature in the 9th attributes field.\n")
+            outfl.write("##gff-version 3\n##Output generated with Pedro script to convert IDs nomenclature in the 9th attributes field.\n")
             geneID=""
             transcriptID=""
             for line in infl:
@@ -17,6 +32,10 @@ def readAugustusGff(infile,outfile):
                     fields=line.split("\t")
                     if fields[2] == "gene":
                         geneID=fields[8].rstrip()
+                        if fields[0] in dict:
+                            outfl.write("##sequence-region " + fields[0] + " 1 " + dict[fields[0]])
+                        else:
+                            print("%s seq id not in genome table. Sequence region will not be present for this reference sequence." % fields[0])
                         outfl.write('\t'.join(fields[:-1]) + '\tID=' + geneID + "\n")
                     elif fields[2] == "transcript":
                         transcriptID=fields[8].rstrip()
@@ -35,8 +54,11 @@ def main():
     parser = argparse.ArgumentParser(description='Script to convert native non gff3 augustus format into a readable gff3.')
     parser.add_argument(dest='inputgff', metavar='augustusGff', help='Input file.')
     parser.add_argument(dest='outputfgff', metavar='outgff3', help='Name of the output file.')
+    parser.add_argument("-g", metavar='-genomeTable', require=True,help="Genome table file to ouptut ##sequence-region in the gff. Advised by genome validation tools.")
     args = parser.parse_args()
-    readAugustusGff(args.inputgff,args.outputfgff)
+
+    outdict = createGenomeTableDict(args.g)
+    readAugustusGff(args.inputgff,args.outputfgff,outdict)
 
 
 if __name__ == "__main__":
