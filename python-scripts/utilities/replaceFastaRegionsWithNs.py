3c8ef33a183c83e0196bfccced5b6c0713ba236e
gnsbr <genosuber.bioinfo@gmail.com>
2017-08-30 10:29:29 +0100

    replace fasta with Ns fix

--- python-scripts/utilities/replaceFastaRegionsWithNs.py
+++ python-scripts/utilities/replaceFastaRegionsWithNs.py
@@ -8,24 +8,33 @@ def replaceFasta(infasta,inbed,outfasta):
         for line in inb:
             if not line.startswith("#"):
                 fields=line.rstrip().split("\t")
-                dict_bed[fields[0]].append((fields[1],fields[2]))
+                dict_bed[fields[0]].append((int(fields[1]),int(fields[2])))
     inb.close()
-    for k, v in dict_bed.items():
-        print(k,v)
-
-
     handle = open(infasta,"rU")
     handleout = open(outfasta,"w")
+    before=0
+    after=0
+    noNs=0
     for seq_record in SeqIO.parse(handle,"fasta"):
         header = seq_record.description
         if header not in dict_bed.keys():
-            handleout.write(header + "\n" + seq_record.seq + "\n")
+            handleout.write(header + "\n" + str(seq_record.seq) + "\n")
         else:
-            seqToEdit=seq_record.seq
             for adaptor in dict_bed[header]:
-                seqToEdit = seqToEdit.replace(seqToEdit[adaptor[0]:adaptor[1]],"N")
-                print(seqToEdit)
+                Ns = "N" * (adaptor[1]- adaptor[0])
+                edited = "".join((str(seq_record.seq[:adaptor[0]]),Ns,str(seq_record.seq[adaptor[1]:])))
+                upstream = int(adaptor[0] - 5)
+                downstream = int(adaptor[1] + 5)
+                if "NNNNN" in seq_record.seq[upstream:adaptor[0]]:
+                    before+=1
+                elif "NNNNN" in seq_record.seq[adaptor[1]:downstream]:
+                    after+=1
+                else:
+                    noNs+=1
 
+                print("".join((header, "     ",str(seq_record.seq[upstream:adaptor[0]]), "     ",Ns, "     ", str(seq_record.seq[adaptor[1]:downstream]))))
+    print("%s:%i\n%s:%i\n%s:%i" % ("Regions with 5 continuous Ns before the region replaced",before,"Regions with 5 continuous Ns after the region replaced",
+                                   after, "Regions with no 5 Ns surrounding the replaced region",noNs))
 
 def main():
     parser = argparse.ArgumentParser(description='Script to replace stretches of a genome with undetermined nucleotides (Ns) based on a bed file. [Done this script'
@@ -34,11 +43,7 @@ def main():
     parser.add_argument(dest='bedfile', metavar='bedFile', help='Input bed file.')
     parser.add_argument(dest='outfile', metavar='outFile', help='Output file.')
     args = parser.parse_args()
-
     replaceFasta(args.fastafile,args.bedfile,args.outfile)
-
-
-
 
 if __name__ == "__main__":
     main()
