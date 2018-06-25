b849e3b2e5acb4e26e1a4646824087c7de4f90f0
pedro <psbpedrobarbosa@gmail.com>
2018-06-25 18:06:29 +0100

    vcf2excel

--- python-scripts/lobo/coverageAnalysis/perTargetCoverageBoxPlot.py
+++ python-scripts/lobo/coverageAnalysis/perTargetCoverageBoxPlot.py
@@ -8,54 +8,54 @@ import numpy as np
 
 
 
-# df = pd.DataFrame(columns=["coverage", "gene","type"])
-# for f in range(1,3):
-#     if f == 1:
-#         type="exon"
-#     else:
-#         type="intron"
-#     feat_dict = {}
-#     feat_name = ""
-#     aux = []
-#     print("Reading file {}".format(f))
-#     with open(sys.argv[f],'r') as infile:
-#         for line in infile:
-#             if not line.startswith("chrom") and not line.startswith("##"):
-#                 if line.split()[4] != feat_name:
-#                     if not feat_name == "":
-#
-#                         weighted_avg = np.average([x[0] for x in aux], weights=[x[1] for x in aux])
-#                         if feat_name.split("_")[1] in feat_dict.keys():
-#                             feat_dict[feat_name.split("_")[1]].append(weighted_avg)
-#                         else:
-#                             feat_dict[feat_name.split("_")[1]] = [weighted_avg]
-#
-#                     feat_name=line.split()[4]
-#                     aux=[(float(line.split("\t")[6]),int(line.split("\t")[3]))]
-#
-#                 else:
-#                     aux.append((float(line.split("\t")[6]),int(line.split("\t")[3])))
-#
-#         if not feat_name == "":
-#             weighted_avg = np.average([x[0] for x in aux], weights=[x[1] for x in aux])
-#             if feat_name.split("_")[1] in feat_dict.keys():
-#                 feat_dict[feat_name.split("_")[1]].append(weighted_avg)
-#             else:
-#                 feat_dict[feat_name.split("_")[1]] = weighted_avg
-#
-#
-#     infile.close()
-#     print("Appending to dataframe..")
-#     for k,v in feat_dict.items():
-#         for val in v:
-#             df = df.append({
-#                 "coverage": val,
-#                 "gene": k,
-#                 "type": type
-#             }, ignore_index=True)
-#
-#
-# df.to_csv("df.ready.csv")
+df = pd.DataFrame(columns=["coverage", "gene","type"])
+for f in range(1,3):
+    if f == 1:
+        type="exon"
+    else:
+        type="intron"
+    feat_dict = {}
+    feat_name = ""
+    aux = []
+    print("Reading file {}".format(f))
+    with open(sys.argv[f],'r') as infile:
+        for line in infile:
+            if not line.startswith("chrom") and not line.startswith("##"):
+                if line.split()[4] != feat_name:
+                    if not feat_name == "":
+
+                        weighted_avg = np.average([x[0] for x in aux], weights=[x[1] for x in aux])
+                        if feat_name.split("_")[1] in feat_dict.keys():
+                            feat_dict[feat_name.split("_")[1]].append(weighted_avg)
+                        else:
+                            feat_dict[feat_name.split("_")[1]] = [weighted_avg]
+
+                    feat_name=line.split()[4]
+                    aux=[(float(line.split("\t")[6]),int(line.split("\t")[3]))]
+
+                else:
+                    aux.append((float(line.split("\t")[6]),int(line.split("\t")[3])))
+
+        if not feat_name == "":
+            weighted_avg = np.average([x[0] for x in aux], weights=[x[1] for x in aux])
+            if feat_name.split("_")[1] in feat_dict.keys():
+                feat_dict[feat_name.split("_")[1]].append(weighted_avg)
+            else:
+                feat_dict[feat_name.split("_")[1]] = weighted_avg
+
+
+    infile.close()
+    print("Appending to dataframe..")
+    for k,v in feat_dict.items():
+        for val in v:
+            df = df.append({
+                "coverage": val,
+                "gene": k,
+                "type": type
+            }, ignore_index=True)
+
+
+df.to_csv("df.ready.csv")
 df=pd.read_csv("df.ready.csv")
 print(df.shape)
 #df_=pd.concat([df,df2])
@@ -68,4 +68,5 @@ sns.set(style="ticks")
 sns.boxplot(x="gene",y="coverage", data=df,hue="type",saturation=0.8,width=0.8,palette="PRGn")
 #sns.despine(offset=10, trim=True)
 plt.xticks(rotation='vertical')
-plt.savefig("perTarget_depthOfCoverage.pdf", format="pdf")
+plt.show()
+#plt.savefig("perTarget_depthOfCoverage.pdf", format="pdf")
--- python-scripts/vcf-tools/vcf2excel.py
+++ python-scripts/vcf-tools/vcf2excel.py
@@ -8,18 +8,53 @@ def extractFields(vcf,fields):
     samples = vcf_data.samples
     no_ref_genotypes=(1,2)
     sample_based=defaultdict(list)
+
+    for field in vcf_data.header_iter():
+        if field["HeaderType"] == "INFO" and field["ID"] == "ANN":
+            tools = field["Description"].split("Format:")[1][:-1].strip().split("|")
+            print(tools)
+
     for record in vcf_data:
         heterozygous = (record.gt_types == 1).nonzero()[0].tolist()
         hom_alt = (record.gt_types == 2).nonzero()[0].tolist()
         merged=heterozygous+hom_alt
         for i in merged:
-            sample_based[samples[i]].extend(record)
+            var=[record.CHROM,str(record.POS), record.REF, record.ALT[0],record.INFO["ANN"].split(",")[0].split("|")[tools.index('Existing_variation')],
+                 record.INFO["ANN"].split(",")[0].split("|")[tools.index('AF')],
+                 record.INFO["ANN"].split(",")[0].split("|")[tools.index('gnomADg_AF_NFE')],
+                 record.INFO["ANN"].split(",")[0].split("|")[tools.index('GERP')],
+                 record.INFO["ANN"].split(",")[0].split("|")[tools.index('phastCons')],
+                 record.INFO["ANN"].split(",")[0].split("|")[tools.index('phyloP')]]
+            for cons in record.INFO["ANN"].split(","):
+                fields=cons.split("|")
+                var.extend((fields[tools.index('Consequence')],fields[tools.index('IMPACT')],fields[tools.index('SYMBOL')],
+                            fields[tools.index('Feature')],
+                            fields[tools.index('HGVSg')],
+                            fields[tools.index('HGVSc')],
+                            fields[tools.index('HGVSp')],
+                            fields[tools.index('CLIN_SIG')],
+                            ))
+
+            sample_based[samples[i]].append(tuple(var))
+
+
+    with open('excel.tsv','w') as out:
+        out.write("###{}\t{}\t{}\t{}\t{}\t"
+                  "{}\t{}\t{}\t{}\t{}\t"
+                  "{}\t{}\t{}\t{}\t{}\t"
+                  "{}\t{}\t{}\t{}\t{}\n".format("Sample_ID","Chrom","Position","Ref_allele","Alt_allele",
+                                                "rsID", "1000G","gnomeAD_MAF","GERP(>3.6)","phastCons(>0.71)",
+                                                "phyloP(>0.8)","Consequence_1","Impact_1", "Gene_1","Transcript_1",
+                                                "HGVSg","HGVSc","HGVSp","Clinical_Sign_1","Consequence_2..,etc"))
+        for sample,variantSet in sample_based.items():
+            for variant in variantSet:
+                out.write("{}\t{}\n".format(sample,'\t'.join(variant)))
 
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Script to produces excel readable file for annotated variants')
-    parser.add_argument(dest='vcf', help='Path to the vcf')
+    parser = argparse.ArgumentParser(description='Script to produces excel readable file for annotated variants.')
+    parser.add_argument(dest='vcf', help='Path to the vcf. This file mast have been split and normalized before.')
     parser.add_argument('-f','--fields', nargs='+', help='Additional fields to include')
     args = parser.parse_args()
 
