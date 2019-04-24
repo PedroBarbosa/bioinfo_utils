a3b2d9bcc287003f54b3773c050558250eede9a5
PedroBarbosa <psbpedrobarbosa@gmail.com>
2019-04-24 12:31:16 +0100

    hierarchical clustering

---
+++ r-scripts/rnaseq/plots/clusteringHeatmap.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(corrplot)
+library(heatmap.plus)
+args = commandArgs(trailingOnly=TRUE)
+features_counts_data=read.table(args[1], header=TRUE,sep="\t")
+
+df_numbers = features_counts_data[ , -which(names(features_counts_data) %in% c("gene_name"))]
+rownames(df_numbers) = df_numbers[,'Geneid']
+df_numbers[,'Geneid'] <- NULL
+#df_numberic = mutate_all(df_numbers, function(x) as.numeric(as.character(x)))
+cor(df_numbers)
+plotheatmap(data.matrix(df_numbers))
+
+
+plotheatmap <- function(x) {
+  heatmap.plus(x,
+            main = "Genes correlation", # heat map title
+            #density.info="none",  # turns off density plot inside color legend
+            trace="none",         # turns off trace lines inside the heat map
+            margins =c(3,5),     # widens margins around plot
+            col=hmcol,       # use on color palette defined earlier
+            #breaks=col_breaks,    # enable color transition at specified limits
+            dendrogram="col",     # only draw a row dendrogram
+            Colv=TRUE, 
+            Rowv = FALSE # turn off column clustering
+            #,scale="row"
+  )   
+}
